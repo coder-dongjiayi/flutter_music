2584dcb5f65bf6da0a4d2511a3c0b8d0491c77a5
董家祎 <575801327@qq.com>
2020-03-12 21:36:21 +0800

    浏览页面

--- lib/browse/browse_banner_widget.dart
+++ lib/browse/browse_banner_widget.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_music/common/music_store.dart';
+import 'package:provider/provider.dart';
+
+class BanerScrollState extends ChangeNotifier{
+  int currentIndex = 0;
+  void setIndex(index){
+    currentIndex = index;
+    notifyListeners();
+  }
+}
 
 class BrowseBannerWidget extends StatelessWidget {
+  @override
 
   var _imageList = [
     "https://pic.xiami.net/images/album/img78/978/49051469082316.jpg?x-oss-process=image/quality,q_80/format,jpg",
@@ -9,28 +20,127 @@ class BrowseBannerWidget extends StatelessWidget {
     "https://pic.xiami.net/images/collect/419/19/30042419_1410842309_hiFG.jpg?x-oss-process=image/quality,q_80",
     "https://pic.xiami.net/images/collect/844/44/361042844_5a3cb6d0baa1d_bKum_1513928400.jpg?x-oss-process=image/quality,q_80"
   ];
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(left: 20,right: 20,top: 30),
 
-      child: _swiperItem()
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<BanerScrollState>(create: (_)=>BanerScrollState())
+      ],
+
+      child: Consumer<BanerScrollState>(
+        builder: (context,state,_){
+          return Column(
+            children: <Widget>[
+              _swiperList(),
+              _swiperControlList()
+            ],
+          );
+        },
+      )
+    );
+
+  }
+
+  ///轮播图下面的control
+  Widget _swiperControlList(){
+    var _index = 0;
+    return Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: _imageList.map((_){
+            Widget item =  _swiperControlItem(_index);
+            _index ++;
+            return item;
+
+          }).toList()
+      ),
     );
   }
 
-  Widget _swiperItem(){
+///每一个control
+  Widget _swiperControlItem(index){
+
+    return Builder(
+
+      builder: (context){
+
+        return Container(
+          width:  Provider.of<BanerScrollState>(context).currentIndex == index ? 30 : 10,
+          height:   Provider.of<BanerScrollState>(context).currentIndex == index ? 8 : 10,
+          margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color:index ==  Provider.of<BanerScrollState>(context).currentIndex ? Color.fromRGBO(85, 161, 193, 1.0): Color.fromRGBO(223, 230, 235, 1.0)
+          ),
+        );
+      },
+    );
+  }
+
+/// 轮播图
+  Widget _swiperList(){
+    return Builder(
+      builder: (context){
+        return AspectRatio(
+          aspectRatio: 1.8,
+          child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(color:MusicStore.Theme.of(context).shadowColor,offset: Offset(15,15),blurRadius: 10),
+                    BoxShadow(color: Colors.white,offset: Offset(-10,-10),blurRadius: 26)
+
+                  ]
+              ),
+              margin: EdgeInsets.only(left: 20,right: 20,top: 30),
+              padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+              child: Swiper(
+
+                duration: 1000,
+                autoplayDelay: 5000,
+                autoplay: true,
+                itemCount: 4,
+                onIndexChanged: (index){
+
+                  Provider.of<BanerScrollState>(context,listen: false).setIndex(index);
+
+                },
+                itemBuilder: (context,index){
+
+                  return  _swiperItem(index);
+                },
+
+              )
+          ),
+        );
+      },
+    );
+  }
+
+
+  Widget _swiperItem(index){
 
     return Container(
-
+      margin: EdgeInsets.fromLTRB(5, 0, 5, 0),
       decoration: BoxDecoration(
-        color: Colors.yellow
+          borderRadius: BorderRadius.circular(8),
+          image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(_imageList[index])
+          )
       ),
-      child:AspectRatio(
-        aspectRatio: 1.5,
-        child: ClipRRect(
-          child: Image.network(_imageList[1],fit: BoxFit.cover,),
-        ),
-      ),
+
     );
   }
 }
+
+
+
+
+
+
